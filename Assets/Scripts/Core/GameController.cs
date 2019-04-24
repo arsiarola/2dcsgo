7ad45e5f6ce5977b252af99ce747f13bd62b0081
Henri Haapanen <Henrihaapanen@metropolia.fi>
2019-04-24 13:32:03 +0300

    kommentti peruttu

--- Assets/Scripts/Core/GameController.cs
+++ Assets/Scripts/Core/GameController.cs
@@ -129,9 +129,7 @@ namespace Core
                 DisableRecordableChildren(ref child);
             }
         }
-        /// <summary>
-        /// Sets all recordables and their children to inactive.
-        /// </summary>
+
         public void DisableRecordables()
         {
             List<int> removeIds = new List<int>();
