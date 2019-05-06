f9859283b952cb625169594520e9f086ac6ff4c8
arsia <arsia@metropolia.fi>
2019-05-06 16:59:49 +0300

    camera movement hiiren avulla kondikses ja yleist parannust camera movementtiin

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -2,43 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
-{
+public class CameraMovement : MonoBehaviour {
     private float spacing = 20f;
-    public Vector3 pos;
-    void Start()
-    {
+    private Vector3 pos;
+    private float mouseWheel;
+    private float dragSpeed = 3f;
+    private Vector3 dragOrigin;
+    void Start() {
         Camera.main.orthographic = true;
-        pos = transform.position;
- }
+    }
 
     // Update is called once per frame
-    void Update()
-    {
-        MoveCamera(); 
-        
+    void Update() {
+        pos = transform.position;
+        mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+        MoveCamera();
     }
     private void MoveCamera() {
+
+        ///if shift is held every camera movement faster
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
             spacing = 60f;
         else
             spacing = 20f;
+
+        /// wasd movement for the camera
         if (Input.GetKey(KeyCode.W))
             pos.y += spacing * Time.unscaledDeltaTime;
-        else if (Input.GetKey(KeyCode.A))
-            pos.x -= spacing * Time.unscaledDeltaTime;
         else if (Input.GetKey(KeyCode.S))
             pos.y -= spacing * Time.unscaledDeltaTime;
+         if (Input.GetKey(KeyCode.A))
+            pos.x -= spacing * Time.unscaledDeltaTime;
         else if (Input.GetKey(KeyCode.D))
             pos.x += spacing * Time.unscaledDeltaTime;
+        transform.position = pos;
 
-         if (Input.GetKey(KeyCode.Comma) &&  Camera.main.orthographicSize + spacing*Time.unscaledDeltaTime < 20) 
+         /// move camera farther and closer with comma and period
+        if (Input.GetKey(KeyCode.Comma)  && Camera.main.orthographicSize + spacing * Time.unscaledDeltaTime < 40)
             Camera.main.orthographicSize += spacing * Time.unscaledDeltaTime;
-         else if (Input.GetKey(KeyCode.Period) && Camera.main.orthographicSize - spacing*Time.unscaledDeltaTime > 5 )
+        else if (Input.GetKey(KeyCode.Period) && Camera.main.orthographicSize - spacing * Time.unscaledDeltaTime > 5)
             Camera.main.orthographicSize -= spacing * Time.unscaledDeltaTime;
 
-    transform.position = pos;
+        /// move camera farther and closer with mousewheel
+        if (mouseWheel > 0 && (Camera.main.orthographicSize - (spacing * Time.unscaledDeltaTime * 5))  > 5)
+            Camera.main.orthographicSize -= spacing * Time.unscaledDeltaTime * 5;
+        else if (mouseWheel < 0 && (Camera.main.orthographicSize + (spacing * Time.unscaledDeltaTime * 5)) < 40)
+            Camera.main.orthographicSize += spacing * Time.unscaledDeltaTime * 5;
+
+        /// move camera around with pressing middle mouse button and after that moving the mouse around        
+        if (Input.GetMouseButtonDown(2)) {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+        if (!Input.GetMouseButton(2)) {
+            return;
+        }
+
+        Vector3 position = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(position.x * dragSpeed, position.y * dragSpeed, 0);
+
+        transform.Translate(move, Space.World);
     }
+
 }
+
 
 
