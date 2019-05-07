4a14bb15ff679d771fa7f641f67704e8ad3c2baf
Henri Haapanen <Henrihaapanen@metropolia.fi>
2019-05-07 18:37:39 +0300

    added weapon sounds and injected them into the shooting method

--- Assets/Scripts/Misc/SoundAssets.cs
+++ Assets/Scripts/Misc/SoundAssets.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class SoundAssets : MonoBehaviour
 {
+    public Dictionary<string, AudioClip> sounds = new Dictionary<string, AudioClip>();
+    public AudioClip shootSound;
+
     private void Awake()
     {
-        
+        shootSound = (AudioClip)Resources.Load("Assets/sounds/shootSound.mp3", typeof(AudioClip));
+        sounds.Add("shootSound", shootSound);
+
     }
 }
