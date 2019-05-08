cd30058bab0bc0d25cde049506ade31fa4b3f33e
Henri Haapanen <Henrihaapanen@metropolia.fi>
2019-05-08 16:16:26 +0300

    added mixer control script...

---
+++ Assets/Scripts/Misc/MixControls.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Assets.Scripts.Misc
+{
+    public class MixerControls : MonoBehaviour
+    {
+        public AudioMixer masterMixer; 
+
+        public void SetSoundVol(float vol)
+        {
+            masterMixer.SetFloat("masterVol", vol);
+        }
+
+    }
+}
